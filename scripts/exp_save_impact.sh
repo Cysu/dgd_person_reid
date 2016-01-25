229bbb68ee3acbd78bef7af6c96e5ec7d2a489c8
Cysu <st.cysu@gmail.com>
2016-01-25 11:07:58 +0800

    Save impact to a user input joint set

--- scripts/exp_save_impact.sh
+++ scripts/exp_save_impact.sh
@@ -7,15 +7,17 @@ cd $(dirname ${BASH_SOURCE[0]})/../
 DB_DIR=external/exp/db
 
 # Parse arguments.
-if [[ $# -ne 2 ]]; then
-    echo "Usage: $(basename $0) split impact_dir"
+if [[ $# -ne 3 ]]; then
+    echo "Usage: $(basename $0) split impact_dir joint_set"
     echo "    split         Split index"
     echo "    impact_dir    A directory of impact scores numpy files"
+    echo "    joint_set     Joint dataset name, e.g., jstl, leave_cuhk01"
     exit
 fi
 printf -v split_index "%02d" $1
 impact_dir=$2
 prefix=$(basename ${impact_dir})
+joint_set=$3
 
 # Save individual
 for ds in $(ls ${impact_dir}); do
@@ -29,11 +31,11 @@ done
 # Save joint
 python2 tools/save_joint_impact_score.py \
     ${impact_dir} \
-    ${DB_DIR}/jstl_split_${split_index}/train.txt \
-    ${DB_DIR}/jstl_split_${split_index}/${prefix}_train_lmdb
-echo "Saved for jstl train"
+    ${DB_DIR}/${joint_set}_split_${split_index}/train.txt \
+    ${DB_DIR}/${joint_set}_split_${split_index}/${prefix}_train_lmdb
+echo "Saved for ${joint_set} train"
 python2 tools/save_joint_impact_score.py \
     ${impact_dir} \
-    ${DB_DIR}/jstl_split_${split_index}/val.txt \
-    ${DB_DIR}/jstl_split_${split_index}/${prefix}_val_lmdb
-echo "Saved for jstl val"
+    ${DB_DIR}/${joint_set}_split_${split_index}/val.txt \
+    ${DB_DIR}/${joint_set}_split_${split_index}/${prefix}_val_lmdb
+echo "Saved for ${joint_set} val"
