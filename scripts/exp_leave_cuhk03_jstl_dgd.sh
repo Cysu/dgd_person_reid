b1e9d3046ffbf2db7781d747f4948fefc575af40
Cysu <st.cysu@gmail.com>
2016-01-25 11:29:51 +0800

    JSTL+DGD for leave-one-out

---
+++ scripts/exp_leave_cuhk03_jstl_dgd.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+# Joint single task learning (JSTL) with domain guided dropout (DGD).
+
+# Change to the project root directory. Assume this file is at scripts/.
+cd $(dirname ${BASH_SOURCE[0]})/../
+
+# Set relative paths.
+CAFFE_DIR=external/caffe
+MODELS_DIR=models/leave_cuhk03
+LOGS_DIR=logs/leave_cuhk03
+SNAPSHOTS_DIR=external/exp/snapshots/leave_cuhk03
+
+# Parse arguments.
+if [[ $# -ne 3 ]]; then
+    echo "Usage: $(basename $0) split model weights"
+    echo "    split         Split index"
+    echo "    model         Model name"
+    echo "    weights       Pretrained caffe model weights"
+    exit
+fi
+printf -v split_index "%02d" $1
+model=$2
+weights=$3
+
+# Make directories.
+mkdir -p ${LOGS_DIR}
+mkdir -p ${SNAPSHOTS_DIR}
+
+# Replace the split_index with our specified one in the template solver and
+# template trainval prototxt.
+trainval=${MODELS_DIR}/jstl_dgd_split_${split_index}_${model}_trainval.prototxt
+solver=${MODELS_DIR}/jstl_dgd_split_${split_index}_${model}_solver.prototxt
+sed -e "s/\${split_index}/${split_index}/g" \
+    ${MODELS_DIR}/jstl_dgd_${model}_trainval.prototxt > ${trainval}
+sed -e "s/\${split_index}/${split_index}/g" \
+    ${MODELS_DIR}/jstl_dgd_${model}_solver.prototxt > ${solver}
+
+# Train the net.
+GLOG_logtostderr=1 mpirun -n 2 ${CAFFE_DIR}/build/tools/caffe train \
+    -solver ${solver} -weights ${weights} -gpu 0,1 \
+    2>&1 | tee ${LOGS_DIR}/jstl_dgd_split_${split_index}_${model}.log
+
+# Cleanup.
+rm ${trainval} ${solver}
