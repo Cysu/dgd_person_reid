9cf670ef78fc452302515e9d8a1baac42a20829b
Cysu <st.cysu@gmail.com>
2016-04-02 10:12:32 +0800

    JSTL scripts

--- scripts/exp_ft_jstl.sh
+++ scripts/exp_ft_jstl.sh
@@ -1,46 +1,32 @@
 #!/usr/bin/env bash
-# Fine-tune on a particular dataset with standard dropout from JSTL.
+# Experiments of fine-tuning on each dataset from JSTL.
 
 # Change to the project root directory. Assume this file is at scripts/.
 cd $(dirname ${BASH_SOURCE[0]})/../
 
-# Set relative paths.
-CAFFE_DIR=external/caffe
-MODELS_DIR=models/ft_jstl
-LOGS_DIR=logs/ft_jstl
-SNAPSHOTS_DIR=external/exp/snapshots/ft_jstl
+source scripts/routines.sh
 
-# Parse arguments.
-if [[ $# -ne 4 ]]; then
-    echo "Usage: $(basename $0) dataset split model weights"
-    echo "    dataset       Dataset name"
-    echo "    split         Split index"
-    echo "    model         Model name"
-    echo "    weights       Pretrained caffe model weights"
-    exit
-fi
-dataset=$1
-printf -v split_index "%02d" $2
-model=$3
-weights=$4
+exp='jstl'
 
-# Make directories.
-mkdir -p ${LOGS_DIR}
-mkdir -p ${SNAPSHOTS_DIR}
+# Get JSTL pretrained model
+pretrained_model=$(get_trained_model ${exp} jstl)
 
-# Replace the split_index with our specified one in the template solver and
-# template trainval prototxt.
-trainval=${MODELS_DIR}/${dataset}_split_${split_index}_${model}_trainval.prototxt
-solver=${MODELS_DIR}/${dataset}_split_${split_index}_${model}_solver.prototxt
-sed -e "s/\${split_index}/${split_index}/g" \
-    ${MODELS_DIR}/${dataset}_${model}_trainval.prototxt > ${trainval}
-sed -e "s/\${split_index}/${split_index}/g" \
-    ${MODELS_DIR}/${dataset}_${model}_solver.prototxt > ${solver}
+# Fine-tune on each dataset
+for dataset in cuhk03 cuhk01 prid viper 3dpes ilids; do
+  train_model ${exp} ${dataset} ${pretrained_model}
+done
 
-# Fine-tuning.
-GLOG_logtostderr=1 mpirun -n 2 ${CAFFE_DIR}/build/tools/caffe train \
-    -solver ${solver} -weights ${weights} -gpu 0,1 \
-    2>&1 | tee ${LOGS_DIR}/${dataset}_split_${split_index}_${model}.log
+# Extract features on all datasets
+for dataset in cuhk03 cuhk01 prid viper 3dpes ilids; do
+  trained_model=$(get_trained_model ${exp} ${dataset})
+  extract_features ${exp} ${dataset} ${trained_model}
+done
 
-# Cleanup.
-rm ${trainval} ${solver}
+# Evaluate performance
+for dataset in cuhk03 cuhk01 prid viper 3dpes ilids; do
+  trained_model=$(get_trained_model ${exp} ${dataset})
+  result_dir=$(get_result_dir ${exp} ${dataset} ${trained_model})
+  echo ${dataset}
+  python2 eval/metric_learning.py ${result_dir}
+  echo
+done
