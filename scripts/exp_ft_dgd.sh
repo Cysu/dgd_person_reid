005b8645c603ddb99a7951175ee6c64328790f28
Cysu <st.cysu@gmail.com>
2016-04-07 13:36:37 +0800

    Update training parameters

--- scripts/exp_ft_dgd.sh
+++ scripts/exp_ft_dgd.sh
@@ -8,8 +8,19 @@ source scripts/routines.sh
 
 exp='dgd'
 
-# Get JSTL pretrained model
+# Make a model for inference (treat BN as fixed affine layer)
+# to fast the neuron impact scores computation
 pretrained_model=$(get_trained_model ${exp} jstl)
+python2 ${CAFFE_DIR}/python/gen_bn_inference.py \
+  models/jstl/jstl_deploy.prototxt ${pretrained_model}
+inference_model=$(get_trained_model_for_inference jstl jstl)
+
+# Compute neuron impact scores (NIS) for each dataset
+# for dataset in cuhk03 cuhk01 prid viper 3dpes ilids shinpuhkan; do
+for dataset in prid; do
+  compute_neuron_impact_scores ${dataset} ${inference_model}
+done
+
 
 # Fine-tune on each dataset
 for dataset in cuhk03 cuhk01 prid viper 3dpes ilids; do
@@ -30,3 +41,4 @@ for dataset in cuhk03 cuhk01 prid viper 3dpes ilids; do
   python2 eval/metric_learning.py ${result_dir}
   echo
 done
+
