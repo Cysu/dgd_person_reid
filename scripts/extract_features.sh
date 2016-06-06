c4ace3d7d7d2033cf9018cc2fb68fdfc81175194
Cysu <st.cysu@gmail.com>
2016-06-06 18:56:30 +0800

    Update the script to extract features

--- scripts/extract_features.sh
+++ scripts/extract_features.sh
@@ -2,56 +2,18 @@
 # Extract features. The batch size is assumed to be 100.
 # Use GPU 0 by default.
 
-# Set relative paths.
-CAFFE_DIR=external/caffe
-DB_DIR=external/exp/db
-RESULTS_DIR=external/exp/results
-MODEL_TEMPLATE=models/exfeat_template.prototxt
+# Change to the project root directory. Assume this file is at scripts/.
+cd $(dirname ${BASH_SOURCE[0]})/../
 
-parse_args() {
-  task=$1
-  dataset=$2
-  weights=$3
-  if [[ $# -eq 4 ]]; then
-    blob=$4
-  else
-    blob=fc7_bn
-  fi
-}
+source scripts/routines.sh
 
-get_output_dir() {
-  parse_args $@
-  weights_name=$(basename $weights)
-  weights_name="${weights_name%%.*}"
-  output_dir=${RESULTS_DIR}/${task}/${dataset}_${weights_name}_${blob}
-  echo ${output_dir}
-}
+if [[ $# -ne 3 ]] && [[ $# -ne 4 ]]; then
+  echo "Usage: $(basename $0) exp dataset weights [blob=fc7_bn]"
+  echo "    exp      Subfolder name to store the extracted features"
+  echo "    dataset  Dataset name (3dpes, cuhk01, cuhk03, ilids, prid, viper)"
+  echo "    weights  Trained caffemodel"
+  echo "    blob     Features blob name (fc7_bn by default)"
+  exit
+fi
 
-extract_features() {
-  parse_args $@
-  output_dir=$(get_output_dir $@)
-
-  # Extract train, val, test probe, and test gallery features.
-  rm -rf ${output_dir}
-  mkdir -p ${output_dir}
-
-  for subset in train val test_probe test_gallery; do
-    echo "Extracting ${subset} set"
-    num_samples=$(wc -l ${DB_DIR}/${dataset}/${subset}.txt | awk '{print $1}')
-    num_samples=$((num_samples + 1))
-    num_iters=$(((num_samples + 99) / 100))
-    model=$(mktemp)
-    sed -e "s/\${dataset}/${dataset}/g; s/\${subset}/${subset}/g" \
-      ${MODEL_TEMPLATE} > ${model}
-    ${CAFFE_DIR}/build/tools/extract_features \
-      ${weights} ${model} ${blob},label \
-      ${output_dir}/${subset}_features_lmdb,${output_dir}/${subset}_labels_lmdb \
-      ${num_iters} lmdb GPU 0
-    python2 tools/convert_lmdb_to_numpy.py \
-      ${output_dir}/${subset}_features_lmdb ${output_dir}/${subset}_features.npy \
-      --truncate ${num_samples}
-    python2 tools/convert_lmdb_to_numpy.py \
-      ${output_dir}/${subset}_labels_lmdb ${output_dir}/${subset}_labels.npy \
-      --truncate ${num_samples}
-  done
-}
+extract_features "$@"
